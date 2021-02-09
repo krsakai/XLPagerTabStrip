c60f0d12686f8e9f0017db3627446e9db5d57352
sakai kuniya <k_sakai@waku-2.com>
2021-02-09 09:53:55 +0900

    SwiftPM

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XLPagerTabStrip",
+            path: "Sources",
             dependencies: []),
         .testTarget(
             name: "XLPagerTabStripTests",
+            path: "Tests",
             dependencies: ["XLPagerTabStrip"]),
     ]
 )
